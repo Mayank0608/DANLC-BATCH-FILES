e5492be2581b39deabff962a22ba7aad946a0441
Mayank Semwal <145260981+Mayank0608@users.noreply.github.com>
2025-01-21 17:30:39 +0530

    Add files via upload
    This is the Answers of given test question from DANLC ANUDIP

---
+++ TEST/Que2.py
@@ -0,0 +1,10 @@
+#2. Create a 3x3 Matrix with Values Ranging from 2 to 10
+
+# Create the matrix
+
+import numpy as np
+
+matrix = np.arange(2, 11).reshape(3, 3)
+
+print("3x3 matrix with values ranging from 2 to 10:")
+print(matrix)
