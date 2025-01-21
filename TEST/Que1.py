c55af00564017d819b1f46db5a95c7091692fff8
Mayank Semwal <145260981+Mayank0608@users.noreply.github.com>
2025-01-21 17:39:21 +0530

    Answers
    These are the answers of given test under DANLC ANUDIP

---
+++ TEST/Que1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create arrays using basic functions
+zeros = [0] * 10
+ones = [1] * 10
+fives = [5] * 10
+
+# Combine arrays
+Zero_array = np.array(zeros)
+One_array = np.array(ones)
+Fives_array = np.array(fives)
+
+print("Array of Zeros:\n",Zero_array)
+print("Array of Zeros:\n",One_array)
+print("Array of Zeros:\n",Fives_array)
---
+++ TEST/Que3.py
@@ -0,0 +1,9 @@
+#3. Create an Array with Values Ranging from 12 to 38
+import numpy as np
+array = list(range(12, 39))
+
+
+array = np.array(array)
+
+print("Array with values ranging from 12 to 38:")
+print(array)
