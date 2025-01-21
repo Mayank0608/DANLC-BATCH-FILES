3fb60b294e49bbb5e59f086fbbfd9406b71d61fd
Mayank Semwal <145260981+Mayank0608@users.noreply.github.com>
2025-01-21 17:38:13 +0530

    Delete TEST directory

--- TEST/Que3.py
+++
@@ -1,9 +0,0 @@
-#3. Create an Array with Values Ranging from 12 to 38
-import numpy as np
-array = list(range(12, 39))
-
-
-array = np.array(array)
-
-print("Array with values ranging from 12 to 38:")
-print(array)
